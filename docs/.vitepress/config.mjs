c6f29632b4acefcd608023d34f894d1ed16fecb4
puv <dev@puv.bar>
2023-10-21 09:59:56 +0300

    Update docs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/puv/liveSubs' }
     ]
   },
   locales: {
